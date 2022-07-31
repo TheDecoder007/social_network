0b08841b53ad3316e61e294b89d4aaa6e84b2b4f
Ryan Murphy <TheDecoder007@gmail.com>
2022-07-31 13:41:01 -0400

    models for user and thoughts done

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const UserSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: "User needs a name!",
+        trim: true,
+        unique: true
+      },
+      email: {
+        type: String,
+        required: "User needs an email!",
+        trim: true,
+        lowercase: true,
+        unique: true,
+        validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/,
+      "Please fill a valid email address",
+    ],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Thought",
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      // prevents virtuals from creating duplicate of _id as `id`
+      id: false,
+    }
+
+
+
+)
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friend.reduce(
+    (total, friend) => total + friend.username.length + 1,
+    0
+  );
+});
+
+const User = model("User", UserSchema);
+module.exports = User;
